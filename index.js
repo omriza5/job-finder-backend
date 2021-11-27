8a89365e49930a99976a6ec5b28cd166af9a53ce
omri zaher <omriza5@gmail.com>
2021-11-27 19:12:23 +0200

    add: cors package

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const users = require("./routes/user.route");
 
 /** mongodb connection */
 require("./db/mongoose")();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", users);
 
